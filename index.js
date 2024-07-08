864733eb8e9cc80cac42dd8546eb02f180bb65bc
CecilXVO <cvarga2@ivytech.edu>
2024-07-08 15:19:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 3030;
 
-app.get('/', async (req, res) => {
-  const username = req.query.username || 'myogeshchavan97';
-  try {
-    const result = await axios.get(
-      `https://api.github.com/users/${username}/repos`
-    );
-    const repos = result.data
-      .map((repo) => ({
-        name: repo.name,
-        url: repo.html_url,
-        description: repo.description,
-        stars: repo.stargazers_count
-      }))
-      .sort((a, b) => b.stars - a.stars);
-
-    res.send(repos);
-  } catch (error) {
-    res.status(400).send('Error while getting list of repositories');
-  }
-});
+// your code
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
