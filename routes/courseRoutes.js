09dab254bb7c0f83b42f8d4e0df341bb316dd5ba
iakerber <iakerber@indiana.edu>
2024-07-21 19:49:28 -0400

    update paths - not sure if correct

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -4,11 +4,11 @@ const courseController = require('../controllers/courseController');
 const router = express.Router();
 
 // course routes
-router.get('/', courseController.course_index);
-router.post('/', courseController.course_create_post);
-router.get('/create', courseController.course_create_get);
-router.get('/:id', courseController.course_details);
-router.put('/:id', courseController.course_update);
-router.delete('/:id', courseController.course_delete);
+router.get('/courses', courseController.course_index);
+router.post('/course_create', courseController.course_create_post);
+router.get('/course_create', courseController.course_create_get);
+router.get('/courses/:id', courseController.course_details);
+router.put('/courses/:id', courseController.course_update);
+router.delete('courses/:id', courseController.course_delete);
 
 module.exports = router;
