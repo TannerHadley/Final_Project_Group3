5a6cc448657dfeb81c89bd5d5f67aed59b55dc62
iakerber <iakerber@indiana.edu>
2024-07-21 19:25:12 -0400

    Update coursedB to add correct fields

--- models/course.js
+++ models/course.js
@@ -6,11 +6,15 @@ const courseSchema = new Schema({
         type: String,
         required: true
     },
-    course_number: {
+    course_subject: {
         type: String,
         required: true
     },
     course_description: {
+        type: String,
+        required: true
+    },
+    course_credits: {
         type: String,
         required: true
     }
