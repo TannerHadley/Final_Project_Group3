dbb3be78d636f2d68e8c116147117bd46efbf8b9
iakerber <iakerber@indiana.edu>
2024-07-21 20:20:02 -0400

    deleted routes and pointed to routes folder

--- server.js
+++ server.js
@@ -6,11 +6,15 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const ejs = require('ejs'); // Make sure to require ejs
 const Course = require('./models/course');
+const courseRoutes = require('.routes/courseRoutes');
+
+const app = express();
 
 // Middleware & static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(express.json());
 
 // Register view engine
 app.set('view engine', 'ejs');
@@ -33,73 +37,7 @@ app.get('/', (req, res) => {
     res.redirect('index_FinalProjectGroup3.html');
 });
 
-app.get('/courses', (req, res) => {
-    Course.find().sort({ createdAt: -1 })
-        .then((result) => {
-            ejs.renderFile(path.join(__dirname, 'views', 'course_index.ejs'), { courses: result }, (err, str) => {
-                if (err) throw err;
-                res.render('layout', {
-                    title: 'All Courses',
-                    body: str
-                });
-            });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-app.post('/courses', (req, res) => {
-    const course = new Course(req.body);
-
-    course.save()
-        .then((result) => {
-            res.redirect('/courses');
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-app.get('/courses/:id', (req, res) => {
-    const id = req.params.id;
-
-    Course.findById(id)
-        .then(result => {
-            ejs.renderFile(path.join(__dirname, 'views', 'course_details.ejs'), { course: result }, (err, str) => {
-                if (err) throw err;
-                res.render('layout', {
-                    title: 'Course Details',
-                    body: str
-                });
-            });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-app.delete('/courses/:id', (req, res) => {
-    const id = req.params.id;
-
-    Course.findByIdAndDelete(id)
-        .then(result => {
-            res.json({ redirect: '/courses' });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-app.get('/course_create', (req, res) => {
-    ejs.renderFile(path.join(__dirname, 'views', 'course_create.ejs'), {}, (err, str) => {
-        if (err) throw err;
-        res.render('layout', {
-            title: 'Add a new course',
-            body: str
-        });
-    });
-});
+app.use('/courses', courseRoutes);
 
 // 404 page
 app.use((req, res) => {
